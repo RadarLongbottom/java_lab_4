a093eb8c897e0c792f17db2aa2fc94ae1e92fd55
RadarLongbottom <aleksander.skierka@gmail.com>
2022-01-25 18:58:42 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,15 +13,14 @@ public class Main {
            - wprowadzeniem innego znaku niż liczba,
         d) użyć metody finally aby wyświetlić komunikat zakończenia programu
         e) wprowadzanie indeksu niech będzie zapętlone do momentu wprowadzenia poprawnego indeksu
-
            PODPOWIEDZI:
            scan.next() - można wprowadzać dowolny znak z klawiatury,
            index = Integer.parseInt(scan.next()); - parsowanie zmiennej do typu int (liczbowego calkowitego)
-
            warto stworzyć flagę np. isIndexCorrect = false, i niech pętla sie wykonuje, dopuki flaga jest false,
            boolean isIndexCorrect
            white(!isIndexCorrect) {}
         */
+
 
 
         /* zad.2
@@ -27,6 +28,48 @@ public class Main {
         b) stworzyć dowolny przykład z ArithmeticException ,
         c) stworzyć dowolny przykład z 2 lub 3 blokami catch i z blokiem finally,
          */
+
+        Scanner scan = new Scanner(System.in);
+
+
+        int[] tablica = new int[6];
+        tablica[0] = 1;
+        tablica[1] = 2;
+        tablica[2] = 3;
+        tablica[3] = 4;
+        tablica[4] = 5;
+        tablica[5] = 6;
+
+
+
+        for( ; ;) {
+
+            System.out.print("Wprowadź liczbę aby wyświetlić tablice: " );
+
+
+            try {
+                int i = scan.nextInt();
+                System.out.println(tablica[i]);
+            } catch (InputMismatchException ex) {
+                System.out.println("Proszę wprowadzić liczbę");
+                break;
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Liczba zbyt duża");
+                break;
+            } finally {
+                System.out.println("\n");
+            }
+        }
+
+
+
+
+
+
+
+
+
 
     }
 }
