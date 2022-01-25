26ad49b346e60f804bc7114fcd70a041a2b957ec
RadarLongbottom <aleksander.skierka@gmail.com>
2022-01-25 15:00:37 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-
-        /* zad.1
-        a) Stwożyć interfejs Budynek,
-        b) interfejs powinien mieć metody:
-            - double powierzchnia(double powBudynku, double powPodworka, double powParkingu);
-            - String adresBudynku(String miejscowosc, String ulica, int nrDomu, String nrLokalu, String kodPocztowy);
-            - void liczbaOkien(int value); // użycie jak setter
-            - void liczbaMieszkancow(int value); // użycie jak setter
-            - void kolorDomu(KolorEnum kolor); // użycie jak setter
-            - double wyliczCene(double powierzchnia, double cena);
-        c) stworzyć enum KolorEnum z przykładowymi kolorami,
-        d) stworzyć jedną metodę z 2 parametrami wymyśloną przez siebie,
-        */
-
-        /* zad.2
-        a) napisać klasę Dom implementującą interfejs Budynek,
-        b) zaimplementować wszystkie metody,
-        c) stworzyć brakujące pola(prywatne), gettery i settery, napisać metodę toString
-        (gettery i settery najlepiej wygenerować: Code/Prawy myszy --> Generate --> getters/setters/toString)
-        d) stworzyć jedną dowolną metodę przez siebie (nie deklarujemy tej metody w interfejsie),
-        */
-
         /* zad.3
         a) w klasie Main stworzyć 2 objekty typu Dom deklarując pierwszy objekt Typem Dom,
         drugi objekt typem Budynek,
-        b) wykorzystać wszystkie metody do implementacji pól,
-        c) wyświetlić w konsoli metodę toString (na obu objektach) opisującą zachowanie klasy Dom,
-        d) sprawdzić czy idzie wywołać z obu instancji objektu (deklaracja z interfejsu i z klasy)
-        metodę której nie ma zadeklarowane w interfejsie Budynek, ale jest dopisana w klassie Dom,
+        b) wykorzystać wszystkie metody do implementacji pól
+        c) wyświetlić w konsoli metodę toString (na obu obiektach) opisującą zachowanie klasy Dom
         */
+
+        Dom dom1 = new Dom();
+        Budynek dom2 = new Dom();
+
+        dom1.powierzchnia(70.50);
+        dom1.adresBudynku("Gdańsk, ul. Polna 130");
+        dom1.kolorDomu(KolorEnum.CZERWONY);
+        dom1.liczbaMieszkancow(4);
+        dom1.sredniaWielkoscNaMieszkanca(70.50,4);
+        System.out.println(dom1.toString());
+
+        dom2.powierzchnia(120.35);
+        dom2.adresBudynku("Gdynia, ul. Morska 15");
+        dom2.kolorDomu(KolorEnum.BIALY);
+        dom2.liczbaMieszkancow(5);
+        dom2.sredniaWielkoscNaMieszkanca(120.35,5);
+        System.out.println(dom2.toString());
     }
 }
